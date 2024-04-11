72b5eea3622d700d1000e0d227776e85594594f9
lindatarod85 <lindatarod@gmail.com>
2024-04-11 10:43:46 +0200

    PrivateRoute component not working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { AuthContext } from "./context/AuthContext";
+/*import { AuthContext } from "./context/AuthContext";
 import LoginForm from "./components/user/Login.js";
 import RegisterForm from "./components/user/Register.js";
 import PostsListings from "./components/posts/PostsPage";
@@ -54,4 +54,59 @@ function App() {
   );
 }
 
+export default App;*/
+
+
+
+
+import { AuthContext } from "./context/AuthContext";
+import LoginForm from "./components/user/Login.js";
+import RegisterForm from "./components/user/Register.js";
+import PostsListings from "./components/posts/PostsPage";
+import ProfilesPage from "./components/profiles/ProfilesPage";
+import SingleProfile from "./components/profiles/SingleProfilePage";
+import SinglePost from "./components/posts/SinglePostPage";
+import MyProfile from "./components/profiles/myprofile/MyProfile";
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import PrivateRoute from "./components/private/PrivateRoute";
+
+function App() {
+  const { token } = useContext(AuthContext);
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={token ? <Navigate to="/posts" /> : <LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+
+        <Route element={<PrivateRoute />}>
+        <Route path="/posts" element={<PostsListings />}/>
+        <Route path="/profiles" element={ <ProfilesPage />}/>
+        <Route path="/post/:id" element={ <SinglePost />} />
+        <Route path="/profile/:name" element={ <SingleProfile />} />
+        <Route path="/myprofile" element={ <MyProfile />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
 export default App;
+
+
+
+
+
+
+
+
---
+++ src/components/private/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'; 
+import { Navigate } from 'react-router-dom'; 
+import { AuthContext } from '../../context/AuthContext';
+
+const PrivateRoute = ({ children }) => {
+    const { token } = useContext(AuthContext);
+  
+    if (!token) {
+      return <Navigate to="/" replace />;
+    }
+  
+    return children;
+  };
+
+  export default PrivateRoute;
+
+ 
+
+
+
+
